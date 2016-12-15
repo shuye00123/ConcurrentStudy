b01a5ae26c9f3715f70309798915f28f7fa2638b
shuye00123 <shuye00123@qq.com>
2016-12-15 22:09:28 +0800

    NotifyAndWait

--- src/com/concurrent/study/java/ConditionTest.java
+++ src/com/concurrent/study/java/ConditionTest.java
@@ -8,5 +8,5 @@ import java.util.concurrent.locks.ReentrantLock;
  * Created by shuye on 2016/12/15.
  */
 public class ConditionTest {
-    
+
 }
--- src/com/concurrent/study/java/NotifyAndWait.java
+++ src/com/concurrent/study/java/NotifyAndWait.java
@@ -1,8 +1,44 @@
 package com.concurrent.study.java;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by shuye on 2016/12/13.
  */
 public class NotifyAndWait {
+    private static String[] strings = new String[1];
 
+    public static void main(String[] args){
+        strings[0] = "false";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.MICROSECONDS.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (strings){
+                    strings[0] = "true";
+                    strings.notify();
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (strings){
+                    while (!strings[0].equals("true")){
+                        try {
+                            System.out.println("wait for notify");
+                            strings.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("its runing!");
+                }
+            }
+        }).start();
+    }
 }
