a5f48ef961a4e99cbecd82e3fc09d073754e9f2f
shuye00123 <shuye00123@qq.com>
2016-12-18 16:44:56 +0800

    Semaphore

---
+++ src/com/concurrent/study/java/SemaphoreTest.java
@@ -0,0 +1,32 @@
+package com.concurrent.study.java;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by shuye on 2016/12/18.
+ */
+public class SemaphoreTest {
+    public static void main(String[] args) {
+        final Semaphore semaphore = new Semaphore(2);
+        for (int i = 0; i<5; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println(Thread.currentThread() + "is already");
+                        semaphore.acquire();
+                        System.out.println(Thread.currentThread() + "is running");
+                        TimeUnit.MILLISECONDS.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }finally {
+                        semaphore.release();
+                    }
+
+                }
+            }).start();
+        }
+        System.out.println("main thread end");
+    }
+}
