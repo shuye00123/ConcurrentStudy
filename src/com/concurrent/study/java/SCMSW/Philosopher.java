30648cae32b52dd1f7f92e90a57997046e1b53bb
shuye00123 <shuye00123@qq.com>
2016-12-19 21:09:37 +0800

    SCMSW day1

---
+++ src/com/concurrent/study/java/SCMSW/Philosopher.java
@@ -0,0 +1,78 @@
+package com.concurrent.study.java.SCMSW;
+
+import java.util.Random;
+
+/**
+ * Created by shuye on 2016/12/19.
+ */
+public class Philosopher extends Thread {
+    static class Chopstick{
+        private int id;
+
+        public Chopstick(int id) {
+            this.id = id;
+        }
+    }
+    private Chopstick left, right;
+    private Random random;
+    public Philosopher(Chopstick left, Chopstick right, String name) {
+        setName(name);
+        this.left = left;
+        this.right = right;
+        random = new Random();
+    }
+    /**
+     *5 10 40s
+     *5 100 3min
+     *5 1000 15min+
+     *10 100  6min
+     */
+    public void run() {
+        try{
+            while (true) {
+                System.out.println("philosopher "+Thread.currentThread().getName()+" is thinking.");
+                Thread.sleep(random.nextInt(100));
+                synchronized (left){
+                    synchronized (right){
+                        System.out.println("philosopher "+Thread.currentThread().getName()+" is eating.");
+                        Thread.sleep(random.nextInt(100));
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        Chopstick c1 = new Chopstick(1);
+        Chopstick c2 = new Chopstick(2);
+        Chopstick c3 = new Chopstick(3);
+        Chopstick c4 = new Chopstick(4);
+        Chopstick c5 = new Chopstick(5);
+        Chopstick c6 = new Chopstick(6);
+        Chopstick c7 = new Chopstick(7);
+        Chopstick c8 = new Chopstick(8);
+        Chopstick c9 = new Chopstick(9);
+        Chopstick c10 = new Chopstick(10);
+        Philosopher p1 = new Philosopher(c1, c2, "1");
+        Philosopher p2 = new Philosopher(c2, c3, "2");
+        Philosopher p3 = new Philosopher(c3, c4, "3");
+        Philosopher p4 = new Philosopher(c4, c5, "4");
+        Philosopher p5 = new Philosopher(c5, c6, "5");
+        Philosopher p6 = new Philosopher(c6, c7, "6");
+        Philosopher p7 = new Philosopher(c7, c8, "7");
+        Philosopher p8 = new Philosopher(c8, c9, "8");
+        Philosopher p9 = new Philosopher(c9, c10, "9");
+        Philosopher p10 = new Philosopher(c10, c1, "10");
+        p1.start();
+        p2.start();
+        p3.start();
+        p4.start();
+        p5.start();
+        p6.start();
+        p7.start();
+        p8.start();
+        p9.start();
+        p10.start();
+    }
+}
---
+++ src/com/concurrent/study/java/SCMSW/disorder.java
@@ -0,0 +1,52 @@
+package com.concurrent.study.java.SCMSW;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by shuye on 2016/12/19.
+ *
+ *
+ * 突然发现CSDN博客被封了我去他吗
+ *
+ * 七周七并发模型第一天
+ * 本节介绍了如何创建线程，并用java对象的内置锁实现互斥。还介绍了线程与锁模型带来的三个主要危害--竞态条件、死锁和内存可见性，并讨论了一些帮助我们避免危害的准则：
+ *   对共享变量的所有访问都需要同步化；
+ *   读线程和写线程都需要同步化；
+ *   按照约定的全局顺序来获取多把锁；
+ *   持有锁的时间应尽可能短
+ *
+ * java线程之间通信是通过共享内存，静态域实例域和数组元素存储在堆内存中，堆内存线程间共享
+ * 一致性内存模型 > JMM > CPU内存模型
+ *
+ * 线程安全的单例模式？  不一定，volatile？
+ *
+ * 反模式, 是指用来解决问题的带有共同性的不良方法。它们已经经过研究并分类，以防止日后重蹈覆辙，并能在研发尚未投产时辨认出来。
+ */
+public class disorder {
+    static class Work1 implements Runnable{
+        @Override
+        public void run() {
+            i = 1;
+            j = 2;
+            System.out.println(j);
+            System.out.println(i);
+        }
+    }
+    static class Work2 implements Runnable{
+        @Override
+        public void run() {
+            i = 2;
+            j = 1;
+            System.out.println(j);
+            System.out.println(i);
+        }
+    }
+    static int i = 0;
+    static int j = 0;
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Work1());
+        Thread thread2 = new Thread(new Work1());
+        thread1.start();
+        thread2.start();
+    }
+}
