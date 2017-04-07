795dca024569af51427bd8b641b13531303b370f
shuye00123 <shuye00123@qq.com>
2017-04-07 10:40:26 +0800

    add volatile

--- src/com/concurrent/study/java/App.java
+++ src/com/concurrent/study/java/App.java
@@ -4,6 +4,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by shuye on 2017/3/30.
+ * volatile increment is not thread-safe
+ * and if use volatile variable with single operation, it's thread-safe
  */
 public class App {
 
@@ -12,10 +14,11 @@ public class App {
 
     public static void main(String[] args) throws InterruptedException {
         App app = new App();
-        app.doWork();
+        //app.doIncrement();
+        app.doSignature();
     }
 
-    public void doWork() throws InterruptedException {
+    public void doIncrement() throws InterruptedException {
         Thread t1 = new Thread(new Runnable(){
             public void run(){
                 for(int i = 0;i < 10000;i++){
@@ -36,6 +39,46 @@ public class App {
         t1.join();
         t2.join();
         System.out.println("Count is: " + count);
+    }
+
+    public void doSignature() throws InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+            int i = 0;
+            @Override
+            public void run() {
+                while (true){
+                    if (count == 0){
+                        System.out.printf("this is t1, now count is 0, and now is %d turn\n", i);
+                        count = 1;
+                        i++;
+                    }
+                    if (i == 100){
+                        break;
+                    }
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            int i = 0;
+            @Override
+            public void run() {
+                while (true){
+                    if (count == 1){
+                        System.out.printf("this is t2, now count is 1, and now is %d turn\n", i);
+                        count = 0;
+                        i++;
+                    }
+                    if (i == 100){
+                        break;
+                    }
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("done!");
     }
 
 }
